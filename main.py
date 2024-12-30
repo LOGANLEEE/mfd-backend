5f82f49e4339b40853464ba5c0222e8dc3716325
LOGAN-VOIZZIT <loganl@voizzit.com>
2024-12-30 18:14:22 +0400

    init

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"
 
 @app.get("/items/")
 async def read_item(skip: int = 0, limit: int = 10):
+    fake_items_db[skip : skip + limit]
     return fake_items_db[skip : skip + limit]
 
 
